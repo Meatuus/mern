6b6f29c9ef75c63a21f7cb4233e885a7e1c335e0
Tyler <tcameronsmith@gmail.com>
2017-08-19 20:45:54 +1000

    add updae and delete methods

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -11,6 +11,8 @@ class CommentBox extends Component {
 
     this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
     this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    this.handleCommentDelete = this.handleCommentDelete.bind(this);
+    this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
   }
 
   loadCommentsFromServer() {
@@ -21,19 +23,50 @@ class CommentBox extends Component {
   }
 
   handleCommentSubmit(comment) {
-    //add POST request
+    let comments = this.state.data;
+    comment.id = Date.now();
+    let newComments = comments.concat([comment]);
+    this.setState({ data: newComments });
+
+    axios.post(this.props.url, comment)
+      .catch(err => {
+        console.error(err);
+        this.setState({ data: comments });
+      });
+  }
+
+  handleCommentDelete(id) {
+    axios.delete(`${this.props.url}/${id}`)
+    .then(res => {
+      console.log('Comment deleted');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  handleCommentUpdate(id, comment) {
+  //sends the comment id and new author/text to our api
+    axios.put(`${this.props.url}/${id}`, comment)
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   componentDidMount() {
     this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   }
 
   render() {
     return (
       <div style={ style.commentBox }>
         <h2>Comments:</h2>
-        <CommentList data={ this.state.data }/>
+        <CommentList
+          onCommentDelete={ this.handleCommentDelete }
+          onCommentUpdate={ this.handleCommentUpdate }
+          data={ this.state.data }
+        />
         <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
       </div>
     )
