a62da43fb46b1c0a2a50207120b553b6d4e3ffc1
Tyler <tcameronsmith@gmail.com>
2017-08-19 19:10:18 +1000

    finish setting up server and api to db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import CommentBox from './CommentBox';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+ <CommentBox />,
+ document.getElementById('root')
+);
 registerServiceWorker();
